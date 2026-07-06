1acfd0ebef8af1fa86920c707bf20b3d8ddc3527
skyn456-fumika <skyn456@gmail.com>
2026-07-06 15:33:39 +0900

    chore: move frontend api url to env

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let isAuthAlertShown = false;
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8095',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
